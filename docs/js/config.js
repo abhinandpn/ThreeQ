4d15e4d6a1451304dfb4a52edfdff77d9df5405e
abhinandpn <abhinandpn4067@gmail.com>
2026-07-24 03:43:49 +0530

    Update API base URL to production endpoint

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_BASE_URL: 'http://localhost:8080',
+  API_BASE_URL: 'https://threeq-1dnj.onrender.com',
   APP_NAME: 'ThreeQ',
   TAGLINE: '3 Questions. 1 Minute. Every Day.',
 };
